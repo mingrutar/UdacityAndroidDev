8c1f9e449d52db2a1703c390a722fb9c31b25a9f
prirodancoder <rutar.ming@gmail.com>
2016-04-06 19:38:15 -0700

    end of lesson 1

--- app/src/main/java/com/example/mingudacity/mysunshine3/MainActivityFragment.java
+++ app/src/main/java/com/example/mingudacity/mysunshine3/MainActivityFragment.java
@@ -1,23 +1,46 @@
 package com.example.mingudacity.mysunshine3;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    List<String> mForecastItems;
+    ArrayAdapter<String> mAdapter;
+    static String[] testData = new String[] {"Today - Sunny - 88/63",
+            "Tomorrow - Foggy - 78/53",
+            "Wed - Cloudy - 72/65",
+            "Thurs - Rainy - 68/51",
+            "Fri - Foggy - 70/49",
+            "Sat - Sunny - 76/68"};
 
     public MainActivityFragment() {
+        mForecastItems = Arrays.asList( testData );
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        Context context = getActivity();
+
+        mAdapter = new ArrayAdapter<String>(context,
+                R.layout.list_item_forecast,        //file name of list view layout (textView here)
+                R.id.list_item_forecast_textview,   //textView id
+                mForecastItems);                     //List of testData
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mAdapter);
         return rootView;
     }
 }
